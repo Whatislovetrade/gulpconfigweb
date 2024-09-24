197eebbd7ca15636d2fa4e36aaff6d7365353080
Vlad Ivannikov <vladmaslow1@gmail.com>
2024-09-24 19:27:33 +0500

    create default gulpfile-base

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const fileInclude = require('gulp-file-include')
 const sass = require('gulp-sass')(require('sass'))
 const server = require('gulp-server-livereload')
 const clean = require('gulp-clean')
+const fs = require('fs')
 
 const fileIncludeSettings = {
     prefix: '@@',
@@ -13,7 +14,15 @@ const serverOptions = {
     open: true,
 }
 
-gulp.task('includeFiles', function() {
+gulp.task('clean', function(done) {
+    if(fs.existsSync('./dist/')) {
+        return gulp.src('./dist/', {read: false})
+            .pipe(clean({forse: true}))
+    }
+    done()
+})
+
+gulp.task('html', function() {
     return gulp.src('./src/*.html')
         .pipe(fileInclude(fileIncludeSettings))
         .pipe(gulp.dest('./dist'))
@@ -26,12 +35,25 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('./dist/css'));
 })
 
-gulp.task('copyImages', function() {
+gulp.task('images', function() {
     return gulp.src('./src/img/**/*')
         .pipe(gulp.dest('./dist/img/'))
 })
 
-gulp.task('startServer', function() {
+gulp.task('server', function() {
     return gulp.src('./dist/')
         .pipe(server(serverOptions))
 })
+
+gulp.task('watch', function() {
+    gulp.watch('./src/sass/**/*.scss', gulp.parallel('sass'))
+    gulp.watch('./src/**/*.html', gulp.parallel('html'))
+    gulp.watch('./src/img/**/*', gulp.parallel('images'))
+})
+
+gulp.task('default', gulp.series(
+    'clean',
+     gulp.parallel('html', 'sass', 'images'),
+     gulp.parallel('server', 'watch')
+
+))
