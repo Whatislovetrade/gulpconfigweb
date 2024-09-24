a8f427101a37cb561d2c5862bcce2bd3e024d10d
Vlad Ivannikov <vladmaslow1@gmail.com>
2024-09-24 17:09:43 +0500

    save gulp, not using

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp')
 const fileInclude = require('gulp-file-include')
 const sass = require('gulp-sass')(require('sass'))
 const server = require('gulp-server-livereload')
+const clean = require('gulp-clean')
 
 const fileIncludeSettings = {
     prefix: '@@',
